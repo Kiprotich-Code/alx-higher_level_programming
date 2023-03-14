840c801c2294811086c47255fa8b72eca11e028f
Kiprotich-Lawi <kiprotichlawi7483@gmail.com>
2023-03-14 06:08:44 -0700

    Error fix

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -5,4 +5,4 @@ for FirstDigit in range(0, 10):
         if FirstDigit == 8 and SecondDigit == 9:
             print("{}{}".format(FirstDigit, SecondDigit))
         else:
-            print("{}".format(FirstDigit, SecondDigit), end="")
+            print("{}{}".format(FirstDigit, SecondDigit), end=", ")

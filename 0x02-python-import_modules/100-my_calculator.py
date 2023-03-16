7c963a390f84836fd2c48bb6b9d69e63d26a3c7d
Kiprotich-Lawi <kiprotichlawi7483@gmail.com>
2023-03-16 12:36:20 -0700

    Error fix - Task 07

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,22 +1,28 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    from calculator_1 import add, sub, mul, div
-    if len(sys.argv) - 1 != 3:
+from calculator_1 import add, sub, mul, div
+
+
+def arg_calc(argv):
+    n = len(argv) - 1
+    if n != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    operator = sys.argv[2]
-    if operator == '+':
-        result = add(a, b)
-    elif operator == '-':
-        result = sub(a, b)
-    elif operator == '*':
-        result = mul(a, b)
-    elif operator == '/':
-        result = div(a, b)
+        exit(1)
+    a = int(argv[1])
+    op = argv[2]
+    b = int(argv[3])
+    if op == '+':
+        print("{:d} {:s} {:d} = {:d}".format(a, op, b, add(a, b)))
+    elif op == '-':
+        print("{:d} {:s} {:d} = {:d}".format(a, op, b, sub(a, b)))
+    elif op == '*':
+        print("{:d} {:s} {:d} = {:d}".format(a, op, b, mul(a, b)))
+    elif op == '/':
+        print("{:d} {:s} {:d} = {:d}".format(a, op, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-    print("{:d} {:s} {:d} = {:d}".format(a, operator, b, result))
+        exit(1)
+
+
+if __name__ == "__main__":
+    import sys
+    arg_calc(sys.argv)

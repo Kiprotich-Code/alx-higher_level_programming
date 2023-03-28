58f9d6882cd28e0795c6f2c5b7fba78c6fbd6178
Kiprotich Lawi <kiprotichlawi7483@gmail.com>
2023-03-28 05:46:02 -0700

    Update - Task 03

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        div = a / b
-        print("Inside result: {:.1f}".format(div))
+        result = a / b
+        print("Inside result: {:.1f}".format(result))
     except:
-        div = None
-        print("Inside result: {}".format(div))
+        result = None
+        print("Inside result: {}".format(result))
     finally:
-        return div
+        return result

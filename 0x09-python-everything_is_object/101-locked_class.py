56a6d8efbb04194be70f134e3adce29d0ff6b0b3
Lawi Kiprotich <fleeklawi@gmail.com>
2023-04-13 13:17:18 +0300

    Update - Advanced Tasks

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -4,7 +4,8 @@
 
 class LockedClass:
     """
-    Prevent creation of new instance attributes, except if the new instance attribute is called 'first_name'.
+    Prevent creation of new instance attributes, 
+    except if the new instance attribute is called 'first_name'.
     """
 
     __slots__ = ["first_name"]

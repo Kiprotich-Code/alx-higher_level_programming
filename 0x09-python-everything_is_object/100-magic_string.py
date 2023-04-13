b75048d1dc8a83018335a528207363e7c2205893
Lawi Kiprotich <fleeklawi@gmail.com>
2023-04-13 11:21:06 +0300

    Update - Advanced Task

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,3 +1,4 @@
-def magic_string(n):
-    message = "BestSchool"
-    return message * n
+#!/usr/bin/python3
+def magic_string():
+    magic_string.n = getatrr(magic_string, 'n', 0) + 1
+    return ("BestSchool, " * (magic_string.n - 1) + "BestSchool")

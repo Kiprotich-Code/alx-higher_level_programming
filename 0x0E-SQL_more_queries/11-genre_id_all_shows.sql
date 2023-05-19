b1b7649a7cd423bbe02d5eec05be9869fade785f
Lawi Kiprotich <fleeklawi@gmail.com>
2023-05-19 09:02:01 +0300

    Update - Task 011

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,5 @@
+-- Imports the db dump of hbtn_0d_tvshows to MySQL server.
+SELECT `tv_shows`.`title`, `tv_show_genres`.`genre_id`
+	FROM `tv_shows` LEFT JOIN `tv_show_genres`
+	ON `tv_shows`.`id` = `tv_show_genres`.`show_id`
+	ORDER BY `tv_shows`.`title` ASC, `tv_show_genres`.`genre_id` ASC;

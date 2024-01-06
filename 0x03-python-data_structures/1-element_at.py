9e0be87f7bde97c3623fcdb3745aaebeee2dbb9d
Lawi Kiprotich <fleeklawi@gmail.com>
2024-01-06 19:45:56 +0300

    Fix - Invalid output

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx > 0 and idx > len(my_list):
-        return (x for x in my_list[idx])
-    else:
-        return None
+    if 0 <= idx < len(my_list):
+        return (my_list[idx])

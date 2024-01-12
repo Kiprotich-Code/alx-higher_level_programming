a1243346cf1652f20d26817ae543cb5c20a7ab76
Lawi Kiprotich <fleeklawi@gmail.com>
2024-01-12 20:55:29 +0300

    Fix - incorrect output

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    return (x*2 for x in a_dictionary)
+    new_dict = a_dictionary.copy()
+    for x in a_dictionary.keys():
+        new_dict [x] *= 2 
+    return (new_dict)

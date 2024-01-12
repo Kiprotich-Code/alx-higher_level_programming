29b7637cd38df056e84d8bbc29d9818406e46b5f
Lawi Kiprotich <fleeklawi@gmail.com>
2024-01-12 21:12:12 +0300

    Added files

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None or a_dictionary == {}:
+        return None
+    for key, value in a_dictionary.items():
+        if value is max(a_dictionary.values()):
+            return key

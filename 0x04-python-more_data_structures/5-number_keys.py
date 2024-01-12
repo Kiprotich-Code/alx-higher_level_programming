e4bff972f3181218462d79ab329038c9fab7df39
Lawi Kiprotich <fleeklawi@gmail.com>
2024-01-12 19:34:46 +0300

    Task 05

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
     count = 0
-    for count in a_dictionary.items():
+    for count in a_dictionary.items()+1:
         count += 1
     return count

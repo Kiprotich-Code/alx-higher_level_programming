c3c591697788079ac653ca56da91f8e555cf6d5d
Lawi Kiprotich <fleeklawi@gmail.com>
2024-01-30 22:25:43 +0300

    Commit - soln for tasks 0 - last

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"Define class LockedClass."
+
+
+class LockedClass:
+    """
+    Prevent creation of new instance attributes, 
+    except if the new instance attribute is called 'first_name'.
+    """
+
+    __slots__ = ["first_name"]
